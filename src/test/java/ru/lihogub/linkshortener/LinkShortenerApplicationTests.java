3cfc938b5830c7bc13785f713f84f21bc409231c
Oleg Lihogub <oligli@yandex.ru>
2022-06-06 22:05:37 +0300

    initial commit

---
+++ src/test/java/ru/lihogub/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lihogub.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
